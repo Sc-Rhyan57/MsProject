a1463b2149a407a12059f4d8ffd6f244a2d204c3
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-04-28 19:04:33 -0300

    Update LoadstringDoorsDeathFarm.lua

--- projects/LoadstringDoorsDeathFarm.lua
+++ projects/LoadstringDoorsDeathFarm.lua
@@ -1,3 +1,6 @@
+local MsdoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/Notification-doorsAPI/refs/heads/main/Msdoors/MsdoorsApi.lua"))()
+
+
 local PlaceId = game.PlaceId
 
 if PlaceId == 6516141723 then
@@ -5,7 +8,17 @@ if PlaceId == 6516141723 then
 end
 
 if PlaceId == 2440500124 then
-  
+MsdoorsNotify(
+    "Wrong place, wrong time...", 
+    "Run the script in the Lobby, not in the game!, 
+    "", 
+    "rbxassetid://6023426923", 
+    Color3.new(0, 1, 0), 
+    10
+    )
+task.wait(5)
+game:GetService("Players").LocalPlayer:Kick("Only run in lobby, not in game!\n https://dsc.gg/betterstar Only run in lobby, not in game! \n Found a Bug? https://dsc.gg/betterstar")
+
 end
 
 print("PlaceId não reconhecido:", PlaceId)
