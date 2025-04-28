f9eb155a5100cea8649b5c2974e246460d15cd20
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-04-28 18:59:22 -0300

    Create LoadstringDoorsDeathFarm.lua

---
+++ projects/LoadstringDoorsDeathFarm.lua
@@ -0,0 +1,11 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 6516141723 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/DoorsDeathFarm/refs/heads/main/Modifiers/mods.lua"))()
+end
+
+if PlaceId == 2440500124 then
+  
+end
+
+print("PlaceId não reconhecido:", PlaceId)
