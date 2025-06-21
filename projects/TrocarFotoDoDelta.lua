9b3636f75fe5fa8b1ec01295060c77ba695af619
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-06-21 18:11:50 -0300

    Create TrocarFotoDoDelta.lua

---
+++ projects/TrocarFotoDoDelta.lua
@@ -0,0 +1,27 @@
+local function BAIXARIMAGEM(githubLink)
+    local deltaimg = "new_logo.png"
+    if isfile(deltaimg) then
+        delfile(deltaimg)
+        print("[ HM ] FOTO DO DELTA JÁ EXISTIA:(APAGADA) " .. deltaimg)
+    end
+    
+    local success, imageData = pcall(function()
+        return game:HttpGet(githubLink)
+    end)
+    
+    if not success then
+        warn("[HM] Falha ao baixar a imagem: " .. githubLink)
+        return false
+    end
+    writefile(fileName, imageData)
+    print("[ pronto ] IMAGEM SALVA! " .. fileName)
+    return true
+end
+
+local linkDaImagem = _G.rhyanDeltaImg
+
+if BAIXARIMAGEM(linkDaImagem) then
+    print("[ HM ] Download concluído com sucesso!")
+else
+    warn("[ HM ] Falha no download da imagem, LLLLLL")
+end
