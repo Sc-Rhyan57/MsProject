066d8146fefda5c8d48c31499ff6e7adf968290a
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-09-07 19:34:02 -0300

    Clone PlayerGui and PlayerScripts to ReplicatedStorage

--- projects/SaveAll.lua
+++ projects/SaveAll.lua
@@ -1,25 +1,23 @@
---[[ MOVE 1 ]]--
 local guifolder = Instance.new("Folder", game:GetService("ReplicatedStorage"))
 guifolder.Name = "Guiee"
 for i, v in game.Players.LocalPlayer.PlayerGui:GetChildren() do
-v.Parent = game.ReplicatedStorage.Guiee
+    local clone = v:Clone()
+    clone.Parent = game.ReplicatedStorage.Guiee
 end
 
---[[ MOVE 2 ]]--
 local pf = Instance.new("Folder", game:GetService("ReplicatedStorage"))
 pf.Name = "pf"
 for i, v in game.Players.LocalPlayer.PlayerScripts:GetChildren() do
-v.Parent = game.ReplicatedStorage.pf
+    local clone = v:Clone()
+    clone.Parent = game.ReplicatedStorage.pf
 end
 
---[[ AGUARDA ]]--
 task.wait(3)
 
---[[ COMEÇA CLONAR ]]--
 local Params = {
  RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
  SSI = "saveinstance",
 }
 local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
-local Options = {} -- Documentation here https://luau.github.io/UniversalSynSaveInstance/api/SynSaveInstance
+local Options = {}
 synsaveinstance(Options)
